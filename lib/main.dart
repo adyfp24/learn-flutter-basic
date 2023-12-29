d99eca6700b2d49b5fe7e1b94cbcf43999bb3cbf
adyfp24 <adyfp24@gmail.com>
2023-12-29 13:45:56 +0700

    belajar widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,21 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('Hello Ady App'),
+          title: Text(
+            'Hello Ady App',
+            style: TextStyle(color: Colors.orange),
+          ),
         ),
         body: Center(
-          child: Text(
-            'Hello Ady',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+          child: Container(
+            width: 200,
+            color: Colors.blue[100],
+            child: Text(
+              'Hello Ady sksksoandididndididididididnnf fififi ffifi', maxLines: 2,
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
