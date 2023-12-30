8a4e02200ae5169d4f7a531f25a575b443a70223
adyfp24 <adyfp24@gmail.com>
2023-12-30 11:25:05 +0700

    belajar styling text

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,21 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[50],
+          title: Text('aplikasi ady', style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30,fontStyle: FontStyle.italic, fontFamily: 'Roboto', color: Colors.blue[100])),
+        ),
         body: Center(
           child: Column(
-            children: [SearchWidget(), SimpleBottomNavigation()],
+            children: [Container(
+              width: 20,
+              height: 20,
+              margin: EdgeInsets.all(10),
+            ), Container(
+              width: 20,
+              height: 20,
+              margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+            )],
           ),
         ),
       ),
@@ -19,104 +32,3 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class SearchWidget extends StatelessWidget {
-  const SearchWidget({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return AppBar(
-      title: Text('Sportify'),
-      actions: [
-        IconButton(
-          onPressed: () {
-            // Tambahkan logika atau navigasi pencarian di sini
-            print('Search button tapped');
-          },
-          icon: Icon(Icons.search),
-        ),
-      ],
-    );
-  }
-}
-
-class SimpleBottomNavigation extends StatefulWidget {
-  const SimpleBottomNavigation({Key? key}) : super(key: key);
-
-  @override
-  State<SimpleBottomNavigation> createState() => _SimpleBottomNavigationState();
-}
-
-class _SimpleBottomNavigationState extends State<SimpleBottomNavigation> {
-  int _selectedIndex = 0;
-  BottomNavigationBarType _bottomNavType = BottomNavigationBarType.fixed;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Your App Title'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("Selected Page: ${_navBarItems[_selectedIndex].label}"),
-          const SizedBox(height: 20),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const SizedBox(width: 10),
-              DropdownButton<BottomNavigationBarType>(
-                hint: Text(_bottomNavType.toString()),
-                items: BottomNavigationBarType.values
-                    .map((item) => DropdownMenuItem(
-                        value: item, child: Text(item.toString())))
-                    .toList(),
-                onChanged: (val) {
-                  if (val == null) return;
-                  setState(() {
-                    _bottomNavType = val;
-                  });
-                },
-              ),
-            ],
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _selectedIndex,
-        selectedItemColor: const Color(0xff6200ee),
-        unselectedItemColor: const Color(0xff757575),
-        type: _bottomNavType,
-        onTap: (index) {
-          setState(() {
-            _selectedIndex = index;
-          });
-        },
-        items: _navBarItems,
-      ),
-    );
-  }
-}
-
-const _navBarItems = [
-  BottomNavigationBarItem(
-    icon: Icon(Icons.home_outlined),
-    activeIcon: Icon(Icons.home_rounded),
-    label: 'Home',
-  ),
-  BottomNavigationBarItem(
-    icon: Icon(Icons.bookmark_border_outlined),
-    activeIcon: Icon(Icons.bookmark_rounded),
-    label: 'Bookmarks',
-  ),
-  BottomNavigationBarItem(
-    icon: Icon(Icons.shopping_bag_outlined),
-    activeIcon: Icon(Icons.shopping_bag),
-    label: 'Cart',
-  ),
-  BottomNavigationBarItem(
-    icon: Icon(Icons.person_outline_rounded),
-    activeIcon: Icon(Icons.person_rounded),
-    label: 'Profile',
-  ),
-];
